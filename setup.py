6fd27abc8b0af2820522a6fdfb1abb72e2722df2
Stephen Barr <stephen@vertexdev.com.au>
2024-08-30 17:18:34 -0700

    Version bump that I should have done in the previous commit

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="clickuphelper",
-    version="0.2.0",
+    version="0.3.0",
     py_modules=["clickuphelper"],
     packages=["clickuphelper"],
     install_requires=["requests", "click"],
